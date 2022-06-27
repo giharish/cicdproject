886cb2e3024f3045fdef0e2abffe2a55c6770869
Girish Raja <graja@vmware.com>
2022-06-27 21:47:12 +0530

    Code Commit

---
+++ src/test/java/com/example/CI/Integration/with/Jenkins/CiIntegrationWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CI.Integration.with.Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiIntegrationWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
