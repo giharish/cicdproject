36901c5530209e2599c1b2f154535d3bebe14582
Girish Raja <graja@vmware.com>
2022-06-27 22:01:29 +0530

    Code Commit

---
+++ src/main/java/com/example/CI/Integration/with/Jenkins/CiIntegrationWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.CI.Integration.with.Jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiIntegrationWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiIntegrationWithJenkinsApplication.class, args);
+	}
+
+}
